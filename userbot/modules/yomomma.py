496bb3f5873fc53161d73feece40ec626e36e0e9
jayrfs <rfsjay@gmail.com>
2021-08-07 19:52:44 +0530

    yo momma so fat she got ratelimited from eating competition yo momma so stupid she ate part of the code and created typos fixed help (i hope),typo

--- userbot/modules/yomomma.py
+++ userbot/modules/yomomma.py
@@ -1,17 +1,22 @@
-import os, requests
+import os, requests, time
 from asyncio.exceptions import TimeoutError
 from userbot import CMD_HELP, bot
 from userbot.events import register
 
 @register(outgoing=True, pattern=r"^\.ym$")
-async def insult(e):
+async def yomomma(e):
     """Yo momma is so fat that she gets group insurance"""
     yomomma=requests.get("https://yomomma-api.herokuapp.com/jokes")
+    if str(yomomma.content)[12:-3]=="Rate limit exceeded: 5 per 1 minute":
+        await e.edit(
+            "**Rate Limited... Waiting 10 second...**")
+        time.sleep(10)
+        yomomma=requests.get("https://yomomma-api.herokuapp.com/jokes")
     await e.edit(str(yomomma.content)[11:-3])
 
 CMD_HELP.update(
     {
-        "yoamma": ">`.yomamma` or >`.ym`"
-        "\nUsage: yomamma"
+        "yo momma": ">`.ym`"
+        "\nUsage:.ym"
     }
 )
