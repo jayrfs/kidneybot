51b7d1c4936dd80b01692e2a30824ee44cee2cb2
jayrfs <rfsjay@gmail.com>
2021-08-11 11:39:10 +0530

    cleaned up ym

--- userbot/modules/yomomma.py
+++ userbot/modules/yomomma.py
@@ -10,13 +10,21 @@ async def yomomma(e):
     if str(yomomma.content)[12:-3]=="Rate limit exceeded: 5 per 1 minute":
         await e.edit(
             "**Rate Limited... Waiting 10 second...**")
-        time.sleep(10)
+        time.sleep(30)
         yomomma=requests.get("https://yomomma-api.herokuapp.com/jokes")
-    await e.edit(str(yomomma.content)[11:-3])
+    insult=str(yomomma.content)[11:-3]
+    insult2=""
+    for i in insult:
+        if i=="\\":
+            continue
+        insult2+=i
+    print(insult2)
+    await e.edit(insult2)
 
 CMD_HELP.update(
     {
-        "yo momma": ">`.ym`"
+        "yo_momma": ">`.ym`\n\n"
+        "sends a yo momma joke.\n can be harsh,\n you have been warned."
         "\nUsage:.ym"
     }
 )
